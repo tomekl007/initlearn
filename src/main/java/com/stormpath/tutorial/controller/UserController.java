6b9cd9c12ae0106444dedfe8b0d9776ddf21c6c8
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-10 19:56:08 +0100

    add respnse eniety

--- src/main/java/com/stormpath/tutorial/controller/UserController.java
+++ src/main/java/com/stormpath/tutorial/controller/UserController.java
@@ -3,6 +3,8 @@ package com.stormpath.tutorial.controller;
 import com.stormpath.sdk.account.Account;
 import com.stormpath.sdk.client.Client;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,9 +19,9 @@ public class UserController {
     private Client client;
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-     public List<Account> getAllUsers(){
+    public ResponseEntity<List<Account>> getAllUsers() {
         List<Account> list = new ArrayList<>();
         client.getAccounts().iterator().forEachRemaining(list::add);
-        return list;
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
