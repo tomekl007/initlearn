e3319b9775f39f20526218f8938d63abb3bc5d47
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-10 22:00:33 +0100

    remove toStr

--- src/main/java/com/stormpath/tutorial/model/User.java
+++ src/main/java/com/stormpath/tutorial/model/User.java
@@ -1,23 +1,10 @@
 package com.stormpath.tutorial.model;
 
-import com.stormpath.sdk.account.AccountStatus;
-
-
 public class User {
     private final String email;
     private final String fullName;
     private final String givenName;
     private final String middleName;
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "email='" + email + '\'' +
-                ", fullName='" + fullName + '\'' +
-                ", givenName='" + givenName + '\'' +
-                ", middleName='" + middleName + '\'' +
-                '}';
-    }
 
     public User(String email, String fullName, String givenName, String middleName) {
         this.email = email;
