794cca45cb134952480d52e1d0fd779d3ead82fe
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-10 23:19:37 +0100

    ysers public

--- src/main/java/com/stormpath/tutorial/model/User.java
+++ src/main/java/com/stormpath/tutorial/model/User.java
@@ -2,17 +2,26 @@ package com.stormpath.tutorial.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown=true)
 public class User {
-    private final String email;
-    private final String fullName;
-    private final String givenName;
-    private final String middleName;
+    public final String email;
+    public final String fullName;
+    public final String givenName;
+    public final String middleName;
 
     public User(String email, String fullName, String givenName, String middleName) {
         this.email = email;
         this.fullName = fullName;
         this.givenName = givenName;
         this.middleName = middleName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", givenName='" + givenName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                '}';
     }
 }
