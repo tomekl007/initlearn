72102b1800a9cf9dbc36dabb148bc0d6d1ee73dc
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-11 11:27:51 +0100

    api

--- src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
@@ -29,7 +29,7 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
         http
             .apply(stormpath()).and()
             .authorizeRequests()
-            .antMatchers("/", "/users**", "/groups**")
+            .antMatchers("/", "/users", "/users/*", "/group/users/*")
             .permitAll();
     }
 }
