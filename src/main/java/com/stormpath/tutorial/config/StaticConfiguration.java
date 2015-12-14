fa9de97fd2d069f6e5c1929e5ad9607f186adbee
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-14 17:55:37 +0100

    a

--- src/main/java/com/stormpath/tutorial/config/StaticConfiguration.java
+++ src/main/java/com/stormpath/tutorial/config/StaticConfiguration.java
@@ -1,7 +1,14 @@
 package com.stormpath.tutorial.config;
 
-/**
- * Created by tomasz.lelek on 14/12/15.
- */
-public class StaticConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class StaticConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/resources/templates/assets/");
+    }
 }
