fe9e29d9e50b7f3e8f24f3bd7635cd851907ac42
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-14 17:45:42 +0100

    a'

--- src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
@@ -18,6 +18,7 @@ package com.stormpath.tutorial.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpath;
@@ -31,5 +32,9 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers("/", "/login", "/users", "/users/*", "/group/users/*", "users/*/*")
             .permitAll();
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/templates/**");
     }
 }
