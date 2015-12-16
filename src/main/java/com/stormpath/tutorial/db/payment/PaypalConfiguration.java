2757af1e8b8a6f59bae2b1915f3d30eb4660bb8f
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-16 13:18:48 +0100

    conf fix

--- src/main/java/com/stormpath/tutorial/db/payment/PaypalConfiguration.java
+++ src/main/java/com/stormpath/tutorial/db/payment/PaypalConfiguration.java
@@ -1,10 +1,12 @@
 package com.stormpath.tutorial.db.payment;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by tomasz.lelek on 16/12/15.
  */
+@Component
 @ConfigurationProperties(prefix = "paypal")
 public class PaypalConfiguration {
     private String url;
