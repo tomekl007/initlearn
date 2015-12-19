3e3536f230cee5b73bd8118866725ac1c8f5d6ef
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-19 12:13:10 +0100

    fiter

--- src/main/java/com/stormpath/tutorial/config/CustomFilter.java
+++ src/main/java/com/stormpath/tutorial/config/CustomFilter.java
@@ -1,10 +1,14 @@
 package com.stormpath.tutorial.config;
 
+import com.stormpath.sdk.servlet.filter.HttpFilter;
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -13,15 +17,15 @@ import java.util.Enumeration;
 /**
  * Created by tomasz.lelek on 18/12/15.
  */
-public class CustomFilter implements Filter {
+public class CustomFilter extends HttpFilter {
     private ServletContext context;
 
-    public void init(FilterConfig fConfig) throws ServletException {
-        this.context = fConfig.getServletContext();
-        this.context.log("RequestLoggingFilter initialized");
-    }
-
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    private static final Logger logger = LoggerFactory.getLogger(CustomFilter.class);    
+    
+    @Override
+    protected void filter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("filter request :" + request.getContextPath());
+        logger.info("filter response :" + response.getStatus());
         chain.doFilter(request, response);
     }
 
--- src/main/java/com/stormpath/tutorial/config/StaticConfiguration.java
+++ src/main/java/com/stormpath/tutorial/config/StaticConfiguration.java
@@ -33,8 +33,8 @@ public class StaticConfiguration extends WebMvcConfigurerAdapter {
         registry.addInterceptor(new LoginInterceptor());
     }
 
-//    @Bean
-//    public Filter shallowEtagHeaderFilter() {
-//        return new CustomFilter();
-//    }
+    @Bean
+    public Filter addFilter() {
+        return new CustomFilter();
+    }
 }
