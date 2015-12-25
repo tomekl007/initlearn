facf6cc13d1d0edb9be2c8e6722edc1f919bee15
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-12-25 17:11:31 +0100

    /register not protected

--- src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
@@ -36,7 +36,7 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
         http
                 .apply(stormpath()).and()
                 .authorizeRequests()
-                .antMatchers("/", "/login", "/users", "/users/**", "/group/users/*", "users/*/*", "users/**/skills", "/isLoggedIn", "/registerFacebookAccount/**")
+                .antMatchers("/", "/login", "/users", "/users/**", "/group/users/*", "users/*/*", "users/**/skills", "/isLoggedIn", "/registerFacebookAccount/**", "/register/**")
                 .permitAll()
                 .and()
                 .antMatcher("/users/**").csrf().disable();
