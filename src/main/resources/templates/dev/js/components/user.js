77acda3b5be48833fdc10d868ab49e5f367ffe06
Bartlomiej Potaczek <bartlomiej.potaczek7@gmail.com>
2016-01-10 20:17:05 +0100

    rerender user profile after adding vote & add disable vote style

--- src/main/resources/templates/dev/js/components/user.js
+++ src/main/resources/templates/dev/js/components/user.js
@@ -5,12 +5,22 @@ import config from '../ajax/config';
 
 var User = React.createClass({
 
+    getInitialState() {
+        return {
+            data: this.props.data
+        };
+    },
     rate(event) {
         /*TODO improve AJAX CALLS*/
 
+        var $rateStar = event.currentTarget;
+        var $rateWrapper = $rateStar.parentNode;
+
         var rate = {
-            rate: parseInt(event.currentTarget.getAttribute('data-rate'))
+            rate: parseInt($rateStar.getAttribute('data-rate'))
         };
+
+        $rateWrapper.classList.add('disable');
 
         $.ajax({
             type: 'post',
@@ -18,8 +28,8 @@ var User = React.createClass({
             data: JSON.stringify(rate),
             headers: config.apiCallHeader(),
             success: function (data) {
-                console.log(data);
-            },
+                this.setState({data: data[0]});
+            }.bind(this),
 
             error: function (jqXHR) {
                 console.log(jqXHR);
@@ -30,45 +40,45 @@ var User = React.createClass({
 
         var userRateInfo;
 
-        var userSkills = this.props.data.skills.map(function (skill, key) {
+        var userSkills = this.state.data.skills.map(function (skill, key) {
             return (
                 <span className='main-user-profile-skill color-white fw-700' key={key}>{skill}</span>
             );
         });
 
-        var userLinks = this.props.data.links.map(function (link, key) {
+        var userLinks = this.state.data.links.map(function (link, key) {
             return (
                 <span className='main-user-profile-link' key={key}>{link}</span>
             );
         });
 
-        if (this.props.data.average !== null) {
+        if (this.state.data.average !== null) {
             userRateInfo =
                 [<p className='main-user-profile-label-average-rate' key={1} >Average rate:</p>,
                 <p className='main-user-profile-average-rate'key={2} >
                     <i className='fa fa-star color-gold'></i>
-                    {this.props.data.average}
+                    {this.state.data.average}
                 </p>,
                 <p className='main-user-profile-label-number-of-rates'key={3} >Number of rates:</p>,
-                <p className='main-user-profile-number-of-rates'key={4} >{this.props.data.numberOfRates}</p>];
+                <p className='main-user-profile-number-of-rates'key={4} >{this.state.data.numberOfRates}</p>];
         }
-        
+
         return (
             <div className='col s12 m6'>
                 <div className='card-wrapper fw-100'>
                     <a className='main-user-profile-nav-profile' href={config.usersHash + this.props.email}></a>
                     <a className='card-header-img bg-white' href={config.usersHash + this.props.email}>
-                        <img src={this.props.data.img} alt='teacher profile image'/>
+                        <img src={this.state.data.img} alt='teacher profile image'/>
                     </a>
                     <div className='card-header'>
-                        <h3 className='main-user-profile-name color-white'>{this.props.data.fullName}</h3>
+                        <h3 className='main-user-profile-name color-white'>{this.state.data.fullName}</h3>
                     </div>
                     <div className='card-content'>
-                        <h3 className='main-user-profile-name color-purple'>{this.props.data.fullName}</h3>
+                        <h3 className='main-user-profile-name color-purple'>{this.state.data.fullName}</h3>
                         <p className='main-user-profile-label-screenhero'>ScreenHero:</p>
-                        <p className='main-user-profile-screenhero'>{this.props.data.screenHero}</p>
+                        <p className='main-user-profile-screenhero'>{this.state.data.screenHero}</p>
                         <p className='main-user-profile-label-rate'>Hour rate:</p>
-                        <p className='main-user-profile-hourRate'>{this.props.data.hourRate}</p>
+                        <p className='main-user-profile-hourRate'>{this.state.data.hourRate}</p>
                         <p className='main-user-profile-label-skills'>Skills:</p>
                         <div className='main-user-profile-skill-list'>
                         {userSkills}
@@ -78,26 +88,26 @@ var User = React.createClass({
                         {userLinks}
                         </div>
                         <p className='main-user-profile-label-bio'>Bio:</p>
-                        <p className='main-user-profile-bio'>{this.props.data.bio}</p>
+                        <p className='main-user-profile-bio'>{this.state.data.bio}</p>
                     {userRateInfo}
-                        <div className='main-user-profile-rating'>
-                            <span className='main-user-profile-rating-star-wrapper' data-rate='5' {...tapOrClick(this.rate)}>
+                        <div className='main-user-profile-rating-wrapper'>
+                            <span className='main-user-profile-rating-star' data-rate='5' {...tapOrClick(this.rate)}>
                                 <i className='fa fa-star-o'></i>
                                 <i className='fa fa-star full-star color-gold'></i>
                             </span>
-                            <span className='main-user-profile-rating-star-wrapper ' data-rate='4' {...tapOrClick(this.rate)}>
+                            <span className='main-user-profile-rating-star ' data-rate='4' {...tapOrClick(this.rate)}>
                                 <i className='fa fa-star-o'></i>
                                 <i className='fa fa-star full-star color-gold'></i>
                             </span>
-                            <span className='main-user-profile-rating-star-wrapper' data-rate='3' {...tapOrClick(this.rate)}>
+                            <span className='main-user-profile-rating-star' data-rate='3' {...tapOrClick(this.rate)}>
                                 <i className='fa fa-star-o'></i>
                                 <i className='fa fa-star full-star color-gold'></i>
                             </span>
-                            <span className='main-user-profile-rating-star-wrapper' data-rate='2' {...tapOrClick(this.rate)}>
+                            <span className='main-user-profile-rating-star' data-rate='2' {...tapOrClick(this.rate)}>
                                 <i className='fa fa-star-o'></i>
                                 <i className='fa fa-star full-star color-gold'></i>
                             </span>
-                            <span className='main-user-profile-rating-star-wrapper' data-rate='1' {...tapOrClick(this.rate)}>
+                            <span className='main-user-profile-rating-star' data-rate='1' {...tapOrClick(this.rate)}>
                                 <i className='fa fa-star-o'></i>
                                 <i className='fa fa-star full-star color-gold'></i>
                             </span>
