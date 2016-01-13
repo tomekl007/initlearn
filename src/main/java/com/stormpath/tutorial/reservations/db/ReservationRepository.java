b5b0d54b979ff57dda885bd62a825f9026dc37e1
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2016-01-13 20:23:25 +0100

    add param annotation

--- src/main/java/com/stormpath/tutorial/reservations/db/ReservationRepository.java
+++ src/main/java/com/stormpath/tutorial/reservations/db/ReservationRepository.java
@@ -2,6 +2,7 @@ package com.stormpath.tutorial.reservations.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     @Query("SELECT r FROM Reservation r WHERE r.teacher = :teacherEmail")
-    List<Reservation> getAllTecherReservations(String teacherEmail);
+    List<Reservation> getAllTecherReservations(@Param("teacherEmail") String teacherEmail);
     @Query("SELECT r FROM Reservation r WHERE r.reserved_by = :reservedBy")
-    List<Reservation> getAllUserAppoitments(String reservedBy);
+    List<Reservation> getAllUserAppoitments(@Param("reservedBy") String reservedBy);
 }
