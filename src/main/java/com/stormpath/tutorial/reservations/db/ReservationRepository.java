33f0b907a4b009711d632904a34ac4b56669c6bf
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2016-01-13 20:14:48 +0100

    define queries

--- src/main/java/com/stormpath/tutorial/reservations/db/ReservationRepository.java
+++ src/main/java/com/stormpath/tutorial/reservations/db/ReservationRepository.java
@@ -1,12 +1,15 @@
 package com.stormpath.tutorial.reservations.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT r FROM Reservation r WHERE r.teacher = :teacherEmail")
     List<Reservation> getAllTecherReservations(String teacherEmail);
+    @Query("SELECT r FROM Reservation r WHERE r.reserved_by = :reservedBy")
     List<Reservation> getAllUserAppoitments(String reservedBy);
 }
