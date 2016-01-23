5618e47562216b50bb1af8889ffe15cd47f40fb5
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2016-01-23 10:55:46 +0100

    csfr

--- src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
@@ -46,6 +46,7 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,  "/login", "/registerFacebookAccount/**", "/register/**", "/registerAccount")
                 .permitAll()
                 .and()
+                .antMatcher("/reservations/**").csrf().disable()
                 .antMatcher("/users/**").csrf().disable()
                 .antMatcher("/register/**").csrf().disable()
                 .antMatcher("/registerAccount").csrf().disable()
