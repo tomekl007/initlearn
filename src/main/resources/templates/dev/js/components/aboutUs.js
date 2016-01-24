f34dab6565d63e32f7453fc400623d940d2b9a34
Bartlomiej Potaczek <bartlomiej.potaczek7@gmail.com>
2016-01-24 23:39:33 +0100

    add intro js

--- src/main/resources/templates/dev/js/components/aboutUs.js
+++ src/main/resources/templates/dev/js/components/aboutUs.js
@@ -6,9 +6,9 @@ class AboutUs extends React.Component {
             <section id='about-us' className='main-section'>
                 <iframe className='main-video' width='100%' height='560px' src='https://www.youtube.com/embed/cSTEB8cdQwo' frameBorder='0' allowFullScreen='true'></iframe>
                 <div className='overlay-content'>
-                    <h2 className='section-header bg-gold color-white'>We are learning programming in pair programming fashion.
+                    <h2 className='section-header bg-gold color-white' data-hint='about us!' data-position='bottom-right-aligned'>We are learning programming in pair programming fashion.
                         <br/>
-                        Each lesson is designed especially for you !
+                        <span data-intro='Hello step one!'>Each lesson is designed especially for you !</span>
                         <br/>
                         <br/>
                         WE'RE COMING SOON
