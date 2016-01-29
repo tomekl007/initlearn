6d3b326e5d4ca2d78059966435a816dafa23443b
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2016-01-29 10:57:19 +0100

    serialzie problems

--- src/main/java/com/stormpath/tutorial/controller/jsonrequest/ReservationAndPayment.java
+++ src/main/java/com/stormpath/tutorial/controller/jsonrequest/ReservationAndPayment.java
@@ -7,8 +7,24 @@ import com.stormpath.tutorial.reservations.db.Reservation;
  * Created by tomasz.lelek on 29/01/16.
  */
 public class ReservationAndPayment {
-    private final Reservation reservation;
-    private final Payment payment;
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public Reservation reservation;
+    public Payment payment;
 
     public ReservationAndPayment(Reservation reservation, Payment payment) {
 
