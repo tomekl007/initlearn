19783367ef72ede23bd2a1f6aa761922f7a9af18
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2016-02-01 21:11:57 +0100

    query fix

--- src/main/java/com/stormpath/tutorial/messages/MessagesRepository.java
+++ src/main/java/com/stormpath/tutorial/messages/MessagesRepository.java
@@ -14,7 +14,7 @@ public interface MessagesRepository extends JpaRepository<MessageDb, Long> {
     @Query("select m from MessageDb m where m.from_email = :email OR m.to_email = :email ORDER BY timestamp desc")
     List<MessageDb> getAllConversationsWith(@Param("email") String conversationParticipantEmail);
 
-    @Query("select m from MessageDb m where (m.from_email = :from_email AND m.to_email = :to_email) OR " +
-            "(m.from_email = :to_email AND m.to_email = :from_email) ORDER BY timestamp desc LIMIT 1")
+    @Query("select m from MessageDb m where ( m.from_email = :from_email AND m.to_email = :to_email ) OR " +
+            "( m.from_email = :to_email AND m.to_email = :from_email ) ORDER BY timestamp desc LIMIT 1")
     MessageDb getLastMessageForConversations(@Param("from_email") String fromEmail, @Param("to_email") String toEmail);
 }
