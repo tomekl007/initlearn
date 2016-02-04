6e7b1cb3360203d7b74d0c8da8d7cea2e8fcf2e4
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2016-02-04 19:34:17 +0100

    verify teacher

--- src/main/java/com/stormpath/tutorial/controller/AdminController.java
+++ src/main/java/com/stormpath/tutorial/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.stormpath.tutorial.controller;
 
+import com.stormpath.sdk.account.Account;
 import com.stormpath.tutorial.model.User;
 import com.stormpath.tutorial.service.AdminService;
+import com.stormpath.tutorial.user.UserService;
 import com.stormpath.tutorial.utils.AccountUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletRequest;
@@ -19,10 +22,12 @@ import java.util.Optional;
 public class AdminController {
     @Autowired
     AdminService adminService;
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/admin/invalidateCache")
     public ResponseEntity<String> invalidateCache(ServletRequest servletRequest) {
-        Optional<User> user = AccountUtils.getUserIfUserLoggedIn(servletRequest).filter(u -> u.email.equals("tomekl007@gmail.com"));//todo make it using PreAuthorize
+        Optional<User> user = getAdminUser(servletRequest);
         if (user.isPresent()) {
             boolean res = adminService.invalidateCaches();
             if (res) {
@@ -33,5 +38,25 @@ public class AdminController {
         } else {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+    }
+
+    @RequestMapping("/admin/verifyTeacher/{email}")
+    public ResponseEntity<String> setTeacherAsVerified(ServletRequest servletRequest, @PathVariable("email") String teacherEmail) {
+        Optional<User> user = getAdminUser(servletRequest);
+        if (user.isPresent()) {
+            Optional<Account> accountByEmail = userService.findAccountByEmail(teacherEmail);
+            if (accountByEmail.isPresent()) {
+                AccountUtils.addIsTeacherVerified(accountByEmail.get(), true);
+                return new ResponseEntity<>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    private Optional<User> getAdminUser(ServletRequest servletRequest) {
+        return AccountUtils.getUserIfUserLoggedIn(servletRequest).filter(u -> u.email.equals("tomekl007@gmail.com"));
     }
 }
